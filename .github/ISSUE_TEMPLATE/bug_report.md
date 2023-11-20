0467e91ac4b8003d48178cb4c277451775ceb1e5
gnzsnz <gonzalosaenz@gmail.com>
2023-11-20 09:03:19 +0100

    Pre commit (#44)
    * pre-commit hooks
    apply linter on code base
    
    * align README with template_README

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -13,27 +13,37 @@ A clear and concise description of what the bug is.
 
 ## To Reproduce
 
-Steps to reproduce the behavior. Please include information related to docker, ex docker run command, .env file, docker-compose.yml.
+Steps to reproduce the behavior. Please include information related to docker,
+ex docker run command, .env file, docker-compose.yml.
 
 Please provide the output of `docker compose config`
 
-## Expected behavior
+## Expected
+
 A clear and concise description of what you expected to happen.
 
 ## Container logs
-If applicable, add the container logs `docker logs <CONTAINER>` or `docker-compose logs` to help explain your problem.
+
+If applicable, add the container logs `docker logs <CONTAINER>` or
+`docker-compose logs` to help explain your problem.
 
 ## Versions
 
 Please complete the following information:
- - OS: [e.g. Windows]
- - Docker version: [e.g. chrome, safari]
- - Image Tag (`docker image inspect ghcr.io/gnzsnz/ib-gateway:tag`): [e.g. latest]
- - Image Digest (`docker images --digests`): [e.g. sha256:60d9d54009b1b66908bbca1ebf5b8a03a39fe0cb35c2ab4023f6e41b55d17894] 
+
+- OS: [e.g. Windows]
+- Docker version: [e.g. chrome, safari]
+- Image Tag (`docker image inspect ghcr.io/gnzsnz/ib-gateway:tag`): [e.g.
+  latest]
+- Image Digest (`docker images --digests`): [e.g.
+  sha256:60d9d54009b1b66908bbca1ebf5b8a03a39fe0cb35c2ab4023f6e41b55d17894]
 
 ## Additional context
+
 Add any other context about the problem here.
 
 What have you tried and failed.
 
-My primary objective is to fix any bug on the container, ex Dockerfile, run.sh script, docker-compose.yml. Please don't expect upstream issues to be solved here (ex. IB gateway, IBC, etc)
+My primary objective is to fix any bug on the container, ex Dockerfile, run.sh
+script, docker-compose.yml. Please don't expect upstream issues to be solved
+here (ex. IB gateway, IBC, etc)
--- stable/scripts/port_forwarding.sh
+++ stable/scripts/port_forwarding.sh
@@ -2,41 +2,39 @@
 
 if [ "$SSH_TUNNEL" = "yes" ]; then
 
-  if [ "$TRADING_MODE" = "paper" ] ; then
-    SSH_LOCAL_PORT=4002
-  else
-    SSH_LOCAL_PORT=4001
-  fi
+	if [ "$TRADING_MODE" = "paper" ]; then
+		SSH_LOCAL_PORT=4002
+	else
+		SSH_LOCAL_PORT=4001
+	fi
 
-  if [ -z "$SSH_REMOTE_PORT" ]; then
-    SSH_REMOTE_PORT="$SSH_LOCAL_PORT"
-  fi
+	if [ -z "$SSH_REMOTE_PORT" ]; then
+		SSH_REMOTE_PORT="$SSH_LOCAL_PORT"
+	fi
 
-  if [ -n "$SSH_VNC_PORT" ] && [ -n "$VNC_SERVER_PASSWORD" ]
-  then
-    SSH_VNC_TUNNEL="-R 127.0.0.1:5900:localhost:$SSH_VNC_PORT"
-  fi
+	if [ -n "$SSH_VNC_PORT" ] && [ -n "$VNC_SERVER_PASSWORD" ]; then
+		SSH_VNC_TUNNEL="-R 127.0.0.1:5900:localhost:$SSH_VNC_PORT"
+	fi
 
-  while true
-  do
-    echo "> ssh sock: $SSH_AUTH_SOCK"
-    bash -c "ssh ${SSH_ALL_OPTIONS} -TNR 127.0.0.1:${SSH_LOCAL_PORT}:localhost:${SSH_REMOTE_PORT} ${SSH_VNC_TUNNEL:-} ${SSH_USER_TUNNEL}"
-    sleep "${SSH_RESTART:-5}"
-  done
+	while true; do
+		echo "> ssh sock: $SSH_AUTH_SOCK"
+		bash -c "ssh ${SSH_ALL_OPTIONS} -TNR 127.0.0.1:${SSH_LOCAL_PORT}:localhost:${SSH_REMOTE_PORT} ${SSH_VNC_TUNNEL:-} ${SSH_USER_TUNNEL}"
+		sleep "${SSH_RESTART:-5}"
+	done
 
 else
-  # no ssh tunnel, start socat
-  sleep 30
-  #
-  if [ "$TRADING_MODE" = "paper" ]; then
-    # paper
-    printf "Forking :::4002 onto 0.0.0.0:4004 > trading mode %s \n" \
-      "${TRADING_MODE}"
-    socat TCP-LISTEN:4004,fork TCP:127.0.0.1:4002
-  else
-    # live
-    printf "Forking :::4001 onto 0.0.0.0:4003 > trading mode %s \n" \
-      "${TRADING_MODE}"
-    socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
-  fi
+	# no ssh tunnel, start socat
+	sleep 30
+	#
+	if [ "$TRADING_MODE" = "paper" ]; then
+		# paper
+		printf "Forking :::4002 onto 0.0.0.0:4004 > trading mode %s \n" \
+			"${TRADING_MODE}"
+		socat TCP-LISTEN:4004,fork TCP:127.0.0.1:4002
+	else
+		# live
+		printf "Forking :::4001 onto 0.0.0.0:4003 > trading mode %s \n" \
+			"${TRADING_MODE}"
+		socat TCP-LISTEN:4003,fork TCP:127.0.0.1:4001
+	fi
 fi
