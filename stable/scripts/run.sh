a8517c714509c90c49780966f9be744a7b711c47
nalon99 <nalon99@hotmail.com>
2023-11-22 10:52:46 +0100

    removed unchanged files from pull request 43

--- stable/scripts/run.sh
+++ stable/scripts/run.sh
@@ -1,58 +1,105 @@
-#!/bin/sh
+#!/bin/bash
 
 export DISPLAY=:1
-export COMMANDSERVERPORT=3999 # this is used later to enable API in TWS
 
+stop() {
+  echo "> 😘 Received SIGINT or SIGTERM. Shutting down IB Gateway."
+
+  #
+  if [ -n "$VNC_SERVER_PASSWORD" ]; then
+    echo "> Stopping x11vnc."
+    pkill x11vnc
+  fi
+  #
+  echo "> Stopping Xvfb."
+  pkill Xvfb
+  #
+  if [ -n "$SSH_TUNNEL" ]; then
+    echo "> Stopping ssh."
+    pkill ssh
+  else
+    echo "> Stopping socat."
+    pkill socat
+  fi
+  # Get PID
+  local pid
+  pid=$(</tmp/pid)
+  # Set TERM
+  echo "> Stopping IBC."
+  kill -SIGTERM "${pid}"
+  # Wait for exit
+  wait "${pid}"
+  # All done.
+  echo "> Done... $?"
+}
+
+# start Xvfb
 rm -f /tmp/.X1-lock
 Xvfb $DISPLAY -ac -screen 0 1024x768x16 &
 
+# setup SSH Tunnel
+if [ "$SSH_TUNNEL" = "yes" ]; then
+
+  _SSH_OPTIONS="-o ServerAliveInterval=${SSH_ALIVE_INTERVAL:-20}"
+  _SSH_OPTIONS+=" -o ServerAliveCountMax=${SSH_ALIVE_COUNT:-3}"
+
+  if [ -n "$SSH_OPTIONS" ]; then
+    _SSH_OPTIONS+=" $SSH_OPTIONS"
+  fi
+  SSH_ALL_OPTIONS="$_SSH_OPTIONS"
+  export SSH_ALL_OPTIONS
+  echo "> SSH options: $SSH_ALL_OPTIONS"
+
+  if [ -n "$SSH_PASSPHRASE" ]; then
+    echo "> Starting ssh-agent."
+    export SSH_ASKPASS_REQUIRE="never"
+    eval "$(ssh-agent)"
+    SSHPASS="${SSH_PASSPHRASE}" sshpass -e -P "passphrase" ssh-add
+    echo "> ssh-agent identities: $(ssh-add -l)"
+  fi
+fi
+
+# start VNC server
 if [ -n "$VNC_SERVER_PASSWORD" ]; then
-  echo "Starting VNC server"
-  /root/scripts/run_x11_vnc.sh &
+  echo "> Starting VNC server"
+  /home/ibgateway/scripts/run_x11_vnc.sh &
 fi
 
-# Start either TWS or IB Gateway
-if [ -z "$GATEWAY_OR_TWS" ]; then
-    # Start TWS by default if not specified
-    GATEWAY_OR_TWS=tws
-    command=
-elif [ "$GATEWAY_OR_TWS" = "gateway" ]; then
-    command='-g'
-elif [ "$GATEWAY_OR_TWS" = "tws" ]; then
-    command=
-else
-    printf "GATEWAY_OR_TWS must be either 'gateway' or 'tws': got '%s'\n" "$GATEWAY_OR_TWS"
-    exit 1
-fi
-
-if [ "$CUSTOM_CONFIG" != "YES" ]; then
+# apply settings
+if [ "$CUSTOM_CONFIG" != "yes" ]; then
   # replace env variables
   envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
 
   # where are settings stored
   if [ -n "$TWS_SETTINGS_PATH" ]; then
     _JTS_PATH=$TWS_SETTINGS_PATH
+    if [ ! -d "$TWS_SETTINGS_PATH" ]; then
+      # if TWS_SETTINGS_PATH does not exists, create it
+      echo "> Creating directory: $TWS_SETTINGS_PATH"
+      mkdir "$TWS_SETTINGS_PATH"
+    fi
   else
     _JTS_PATH=$TWS_PATH
   fi
-
   # only if jts.ini does not exists
   if [ ! -f "$_JTS_PATH/$TWS_INI" ]; then
     echo "Setting timezone in ${_JTS_PATH}/${TWS_INI}"
     envsubst < "${TWS_PATH}/${TWS_INI}.tmpl" > "${_JTS_PATH}/${TWS_INI}"
   fi
-
 fi
 
-# start ssh server
-/root/scripts/ssh_start.sh
+# forward ports, socat or ssh
+/home/ibgateway/scripts/port_forwarding.sh &
 
-/root/scripts/fork_ports_delayed.sh &
-
-/root/scripts/enable_api.sh & # starts in the background and wait for 300 seconds before running a server command to enable API
-
-/root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" $command \
+# start IBC
+/home/ibgateway/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
      "--on2fatimeout=${TWOFA_TIMEOUT_ACTION}" \
-     "--tws-settings-path=${TWS_SETTINGS_PATH:-}"
+     "--tws-settings-path=${TWS_SETTINGS_PATH:-}" &
+
+pid="$!"
+echo "$pid" > /tmp/pid
+trap stop SIGINT SIGTERM
+wait "${pid}"
+exit $?
