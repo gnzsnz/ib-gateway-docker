1de14f3221200d9e57f1a1f7256f82085e98beda
gonzo <gonzalosaenz@gmail.com>
2023-11-22 13:35:12 +0100

    remove added files. align run_x11_vnc Changes to be committed: 	deleted:    image-files/scripts/enable_api.sh 	modified:   image-files/scripts/run_x11_vnc.sh 	deleted:    image-files/scripts/ssh_start.sh

--- image-files/scripts/run_x11_vnc.sh
+++ image-files/scripts/run_x11_vnc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-x11vnc -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD" 
+x11vnc -ncache_cr -display :1 -forever -shared -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD" 
 # shared option allows more than one viewer to connect at the same time
 # forever option tells our VNC server to keep listening for more connections rather than exiting as soon as the first client disconnects
--- image-files/scripts/ssh_start.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && echo $SSH_PUBLIC_KEY >> ~/.ssh/authorized_keys
-service ssh start
