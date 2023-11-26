13a326ab273d2880dc45acca7fe2c40668e713f2
gonzo <gonzalosaenz@gmail.com>
2023-11-26 16:57:25 +0100

    fix ssh-agent env file, store it in $HOME rather than hardcoded home fix socat start after tws restart. don't re-start if already running add messages to run.sh Changes to be committed: 	modified:   latest/scripts/common.sh 	modified:   latest/scripts/port_forwarding.sh 	modified:   latest/scripts/run.sh

--- latest/scripts/run.sh
+++ latest/scripts/run.sh
@@ -2,6 +2,10 @@
 # shellcheck disable=SC2317
 # Don't warn about unreachable commands in this file
 
+echo "*************************************************************************"
+echo ".> Starting IBC/IB gateway"
+echo "*************************************************************************"
+
 # shellcheck disable=SC1091
 source "${SCRIPT_PATH}/common.sh"
 
@@ -38,6 +42,7 @@ stop_ibc() {
 
 start_xvfb() {
 	# start Xvfb
+	echo ".> Starting Xvfb server"
 	DISPLAY=:1
 	export DISPLAY
 	rm -f /tmp/.X1-lock
@@ -72,6 +77,14 @@ set_ports
 # forward ports, socat or ssh
 "${SCRIPT_PATH}/port_forwarding.sh" &
 
+echo ".> Starting IBC with params:"
+echo ".>		Version: ${TWS_MAJOR_VRSN}"
+echo ".>		program: ${IBC_COMMAND:-gateway}"
+echo ".>		tws-path: ${TWS_PATH}"
+echo ".>		ibc-path: ${IBC_PATH}"
+echo ".>		ibc-init: ${IBC_INI}"
+echo ".>		tws-settings-path: ${TWS_SETTINGS_PATH:-$TWS_PATH}"
+echo ".>		on2fatimeout: ${TWOFA_TIMEOUT_ACTION}"
 # start IBC -g for gateway
 "${IBC_PATH}/scripts/ibcstart.sh" "${TWS_MAJOR_VRSN}" -g \
 	"--tws-path=${TWS_PATH}" \
