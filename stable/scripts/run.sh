b699118d11fd913c0d278e7dffd0da5e7a2a13e6
gnzsnz <8376642+gnzsnz@users.noreply.github.com>
2025-07-30 13:34:58 +0200

    Release v10.25.1l-latest
    Signed-off-by: gnzsnz <8376642+gnzsnz@users.noreply.github.com>

--- stable/scripts/run.sh
+++ stable/scripts/run.sh
@@ -3,22 +3,36 @@
 export DISPLAY=:1
 
 rm -f /tmp/.X1-lock
-Xvfb :1 -ac -screen 0 1024x768x16 &
+Xvfb $DISPLAY -ac -screen 0 1024x768x16 &
 
 if [ -n "$VNC_SERVER_PASSWORD" ]; then
   echo "Starting VNC server"
   /root/scripts/run_x11_vnc.sh &
 fi
 
-# replace env variables
-envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
-envsubst < "${TWS_INI}.tmpl" > "${TWS_INI}"
+if [ "$CUSTOM_CONFIG" != "YES" ]; then
+  # replace env variables
+  envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
 
+  # where are settings stored
+  if [ -n "$TWS_SETTINGS_PATH" ]; then
+    _JTS_PATH=$TWS_SETTINGS_PATH
+  else
+    _JTS_PATH=$TWS_PATH
+  fi
+
+  # only if jts.ini does not exists
+  if [ ! -f "$_JTS_PATH/$TWS_INI" ]; then
+    echo "Setting timezone in ${_JTS_PATH}/${TWS_INI}"
+    envsubst < "${TWS_PATH}/${TWS_INI}.tmpl" > "${_JTS_PATH}/${TWS_INI}"
+  fi
+
+fi
 
 /root/scripts/fork_ports_delayed.sh &
 
 /root/ibc/scripts/ibcstart.sh "${TWS_MAJOR_VRSN}" -g \
      "--tws-path=${TWS_PATH}" \
      "--ibc-path=${IBC_PATH}" "--ibc-ini=${IBC_INI}" \
-     "--user=${TWS_USERID}" "--pw=${TWS_PASSWORD}" "--mode=${TRADING_MODE}" \
-     "--on2fatimeout=${TWOFA_TIMEOUT_ACTION}"
+     "--on2fatimeout=${TWOFA_TIMEOUT_ACTION}" \
+     "--tws-settings-path=${TWS_SETTINGS_PATH:-}"
