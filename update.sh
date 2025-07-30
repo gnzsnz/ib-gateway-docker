39e31b1c34b21ec87b60243a32534c9ff1ac90a2
gnzsnz <8376642+gnzsnz@users.noreply.github.com>
2025-07-30 13:34:52 +0200

    Release v10.20.1a-latest
    Signed-off-by: gnzsnz <8376642+gnzsnz@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 2 ] ; then
 	echo "Usage: ./update.sh <channel> <version>"
 	exit 1
 fi
@@ -16,19 +16,8 @@ if [ "$channel" != "stable" ] && [ "$channel" != "latest" ]; then
 	exit 1
 fi
 
-echo ".> Setting channle: $channel and version: $version for ibgateway"
 cp -r image-files/. "$channel/."
-
-# Dockerfile
 rm -f "$channel/Dockerfile"
-# shellcheck disable=SC2016
-VERSION="$version" CHANNEL="$channel" envsubst '$VERSION,$CHANNEL' <"Dockerfile.template" >"$channel/Dockerfile"
+VERSION="$version" CHANNEL="$channel" envsubst '$VERSION,$CHANNEL' < "Dockerfile.template" > "$channel/Dockerfile"
 
-echo ".> Setting channle: $channel and version: $version for tws"
-
-# Dockerfile tws
-rm -f "$channel/Dockerfile.tws"
-# shellcheck disable=SC2016
-VERSION="$version" CHANNEL="$channel" envsubst '$VERSION,$CHANNEL' <"Dockerfile.tws.template" >"$channel/Dockerfile.tws"
-
-echo ".> Done"
+echo "Done"
